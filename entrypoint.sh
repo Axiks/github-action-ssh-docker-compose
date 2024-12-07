3b18284cabc1c7d1fbdfd1a507cc7d5a382b600c
Your Name <you@example.com>
2024-12-08 00:55:49 +0100

    replace repository transfer into repository clone

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,18 +9,16 @@ cleanup() {
   set +e
   log "Killing ssh agent."
   ssh-agent -k
-  log "Removing workspace archive."
-  rm -f /tmp/workspace.tar.bz2
 }
 trap cleanup EXIT
-
-log "Packing workspace into archive to transfer onto remote machine."
-tar cjvf /tmp/workspace.tar.bz2 --exclude .git --exclude vendor .
 
 log "Launching ssh agent."
 eval `ssh-agent -s`
 
-remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Removing workspace...'; rm -rf \"\$HOME/workspace\" ; } ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/workspace\" ;  log 'Unpacking workspace...' ; tar -C \"\$HOME/workspace\" -xjv ; log 'Launching docker compose...' ; cd \"\$HOME/workspace\" ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up --remove-orphans --build; log 'Launch!'"
+REPO_NAME=${GITHUB_REPOSITORY}
+GITHUB_TOKEN=${GITHUB_TOKEN}  
+
+remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ;  log 'Clone repository...' ; git clone https://oauth2:${GITHUB_TOKEN}@github.com/${REPO_NAME}.git; log 'Launching docker compose...' ; cd \"\$HOME/\$REPO_NAME\" ; docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up --remove-orphans --build; log 'Launch!'"
 
 if $USE_DOCKER_STACK ; then
   remote_command="set -e ; log() { echo '>> [remote]' \$@ ; } ; cleanup() { log 'Removing workspace...'; rm -rf \"\$HOME/workspace\" ; } ; log 'Creating workspace directory...' ; mkdir -p \"\$HOME/workspace/$DOCKER_COMPOSE_PREFIX\" ; trap cleanup EXIT ; log 'Unpacking workspace...' ; tar -C \"\$HOME/workspace/$DOCKER_COMPOSE_PREFIX\" -xjv ; log 'Launching docker stack deploy...' ; cd \"\$HOME/workspace/$DOCKER_COMPOSE_PREFIX\" ; docker stack deploy -c \"$DOCKER_COMPOSE_FILENAME\" --prune \"$DOCKER_COMPOSE_PREFIX\""
@@ -34,5 +32,4 @@ ssh-add <(echo "$SSH_PRIVATE_KEY")
 echo ">> [local] Connecting to remote host."
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
   "$SSH_USER@$SSH_HOST" -p "$SSH_PORT" \
-  "$remote_command" \
-  < /tmp/workspace.tar.bz2
+  "$remote_command"
