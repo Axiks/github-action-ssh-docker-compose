1cf7aa55bef28d91c955b99097087015d92abf26
Yuno <36519646+Axiks@users.noreply.github.com>
2025-01-06 16:11:00 +0100

    added the ability to run multiple containers in parallel

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,10 +22,15 @@ REPO_NAME=${GITHUB_REPOSITORY}
 GITHUB_TOKEN=${GITHUB_TOKEN}
 
 FOLDER_NAME="${REPO_NAME##*/}"
+FOLDER_NAME="${FOLDER_NAME}-${REPO_NAME}"
 
 GITHUB_TOKEN_ENCODE=$(python3 /tokenEncode.py "$GITHUB_TOKEN")
 
 git_url="https://oauth2:${GITHUB_TOKEN_ENCODE}@github.com/${REPO_NAME}.git"
+git_branch_name= $GITHUB_BRANCH_NAME #"make-aspire"
+
+env_file_name= $ENV_FILE_NAME #".env"
+path_to_env_file="\$HOME/lumi-config/$env_file_name"
 
 
 remote_command="set -e ; "
@@ -38,14 +43,13 @@ remote_command+=" git pull $git_url ; "
 remote_command+=" git pull ; "
 remote_command+="else "
 remote_command+=" log 'Clone repository...' ; "
-remote_command+=" git clone -b make-aspire $git_url ; "
+remote_command+=" git clone -b $git_branch_name $git_url ; "
 remote_command+=" cd \"\$HOME/${FOLDER_NAME}\" ; "
 remote_command+="fi ; "
-remote_command+="cp \$HOME/lumi-config/.env .env ; "
-# remote_command+="cp \$HOME/lumi-config/appsettings.Docker-Production.json Vanilla.Common/appsettings.Docker-Production.json ; "
+remote_command+="cp $path_to_env_file $env_file_name ; "
 remote_command+="docker compose stop ; "
 remote_command+="log 'Launching docker compose...' ; "
-remote_command+="docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" up --remove-orphans --build --force-recreate ; "
+remote_command+="docker compose -f \"$DOCKER_COMPOSE_FILENAME\" -p \"$DOCKER_COMPOSE_PREFIX\" --env-file $env_file_name up --remove-orphans --build --force-recreate ; "
 
 # echo "$remote_command" >foo.sh
 
